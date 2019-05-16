3c52f5eb6c77c121c939b14b86d38c97e92558b1
MAHOOR\Ebi <zare.math@gmail.com>
2019-05-16 12:28:00 -0400

    developed changing password

--- locallibrary_project/locallibrary_project/urls.py
+++ locallibrary_project/locallibrary_project/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^catalog/', include('catalog.urls')),
-    
+
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
