e26e1c6f541a74d652cfc48769af8cf06cd604af
MAHOOR\Ebi <zare.math@gmail.com>
2019-05-16 10:31:20 -0400

    developed primitive pages

--- locallibrary_project/catalog/urls.py
+++ locallibrary_project/catalog/urls.py
@@ -2,6 +2,12 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 
-urlpatterns=[
+from . import views
 
+app_name='catalog'
+urlpatterns=[
+url(r'^$', views.Index.as_view(), name='index'),
+url(r'^books/$', views.BookListView.as_view(), name='books'),
+url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
 ]
